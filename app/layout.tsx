e20c150952e8a09559b7dec3c19eb682e2ffb2e8
GAGAN RAGHAV <120806356+GAGANRAGHAV@users.noreply.github.com>
2024-04-01 05:27:55 +0530

    test1

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'Campus360',
+  description: '',
   icons: {
     icon: '/assets/images/logo.svg'
   }
